f5bab31bcc6735b95985bf8430ba1bbbfb783ef1
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-04 19:02:54 +0500

    env with values added

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ACCOUNT_ID=099199746132
+export REPOSITORY_URI=099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo-app2
+
 # Login , authenticate your Docker client to your registry
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+
 
 # Pull the Docker image from Docker Hub
-docker pull 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo-app2:latest
+docker pull $REPOSITORY_URI:latest
 
 # Run the Docker image as a container
-docker run -d -p 3001:3000 --name container-app2 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo-app2:latest
+docker run -d -p 3001:3000 --name container-app1 $REPOSITORY_URI:latest
