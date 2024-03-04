438e23a8f3ba7089e125de30101e7f0a39f2e427
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-04 16:25:41 +0500

    demo-repo-app2 added

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,7 +5,7 @@ set -e
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
 
 # Pull the Docker image from Docker Hub
-docker pull 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-project-ecr-0001:latest
+docker pull 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo-app2:latest
 
 # Run the Docker image as a container
-docker run -d -p 3001:3000 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-project-ecr-0001:latest
+docker run -d -p 3001:3000 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo-app2:latest
