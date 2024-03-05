e8be39a43b1ba1c4b729e0c55567f27416db315e
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-05 16:00:50 +0500

    docker-compose updated

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-//const express = require('express');
-//const app = express();
-//const port = process.env.PORT || 3000; // Use the environment variable if available.
-//
-//app.get('/', (req, res) => {
-//  res.send('Hello from App2!.....');
-//});
-//
-//app.listen(port, () => {
-//  console.log(`App2 listening on port ${port}`);
-//});
-
-//######################################
-
 // Require the express module
 const express = require('express');
 // Create an express application
