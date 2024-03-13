ac8a5de0d26f99f9ac0a81c474c32595ba2ed2e2
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-13 14:14:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const express = require('express');
 // Create an express application
 const app = express();
 // Define the port. Use the environment variable PORT if it's available; otherwise, use 3001.
-// Changed the default port to 3001 to avoid conflict with node_app_1
 const port = process.env.PORT || 3001;
 
 // Define a route for GET requests to the root URL ("/")
 app.get('/', (req, res) => {
   // Send a response containing a message
-  res.send('Hello from App2!.....');
+  res.send('Hello from App2!');
+});
+
+// Define a route for GET requests to "/api2"
+app.get('/api2', (req, res) => {
+  // Send a response specific to api2
+  res.send('Response from API2 endpoint');
 });
 
 // Start the application, listening on the defined port
@@ -17,3 +22,4 @@ app.listen(port, () => {
   // Log a message to the console once the application starts listening
   console.log(`App2 listening on port ${port}`);
 });
+
